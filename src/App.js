f02dc70fba264c7a4f19ac96d7be41ada9a4d63c
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2022-02-06 18:33:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import Form from './Form';
+
+class App extends Component {
+  render(){
+    return(
+      <div className='App'>
+        <Form/>
+
+      </div>
+    )
+  }
+}
+
+export default App;
